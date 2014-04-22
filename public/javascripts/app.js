682f66dff97a8d2b98de79e0585ef4e6ea2c6ec2
dingziran <dingziranrr@163.com>
2014-04-22 22:43:25 +0800

    add tab and modal

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var app=angular.module("myApp",['ngRoute','firebase']);
+var app=angular.module("myApp",['ngRoute','firebase','ui.bootstrap']);
 app.config(['$routeProvider',function($routeProvider){
     $routeProvider
         .when('/',{
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -29,55 +29,77 @@ app.controller('LoginController',function($scope){
         alert("it works!");
     };
 });
-app.controller('ProcessController',function($scope,$firebase){
+app.controller('ProcessController',function($scope,$firebase,$modal,$log){
     var processesRef = new Firebase("https://sweltering-fire-3478.firebaseio.com/process");
     $scope.processes = $firebase(processesRef);
-    $scope.process={};
-    $scope.click={value:true,name:"新建过程"};
+    $scope.process={fathers:[],previous:[]};
     $scope.remove = function(key){
         $scope.processes.$remove(key);
-    }
-    $scope.change=function(){
-        if($scope.click.value){
-            $scope.click.value=false;
-            $scope.click.name="显示过程列表";
-        }
-        else{
-            $scope.click.value=true;
-            $scope.click.name="新建过程";
-        }
-    }
+    };
     $scope.create = function(){
         $scope.processes.$add($scope.process).then(function(){
             $scope.process={};
-            $scope.change();
         });
 
-    }
+    };
+    $scope.newFather = {key:null};
+    $scope.open = function () {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'processModalContent.html',
+            controller: 'ProcessModalController',
+            scope:$scope
+        });
+
+        modalInstance.result.then(function () {
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+    $scope.addFather=function(){
+        $log.info("new father is "+$scope.newFather.key);
+        $scope.process.fathers.push($scope.newFather.key);
+        $scope.newFather={};
+    };
 });
-app.controller('ProductController',function($scope,$firebase){
+app.controller('ProcessModalController',function ($scope, $modalInstance) {
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
+app.controller('ProductController',function($scope,$firebase,$modal,$log){
     var productsRef = new Firebase("https://sweltering-fire-3478.firebaseio.com/product");
     $scope.products = $firebase(productsRef);
-    $scope.product={};
-    $scope.click={value:true,name:"新建产品"};
+    $scope.product={items:[]};
+    $scope.newItem={};
     $scope.remove = function(key){
         $scope.products.$remove(key);
-    }
-    $scope.change=function(){
-        if($scope.click.value){
-            $scope.click.value=false;
-            $scope.click.name="显示产品列表";
-        }
-        else{
-            $scope.click.value=true;
-            $scope.click.name="新建产品";
-        }
     }
     $scope.create = function(){
         $scope.products.$add($scope.product).then(function(){
             $scope.product={};
-            $scope.change();
+        });
+    }
+    $scope.addItem=function(){
+        $scope.product.items.push($scope.newItem);
+        $scope.newItem={};
+    }
+    $scope.open = function () {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'productModalContent.html',
+            controller: 'ProductModalController',
+            scope:$scope
         });
 
-    }
+        modalInstance.result.then(function () {
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+});
+app.controller('ProductModalController',function ($scope, $modalInstance) {
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 });
