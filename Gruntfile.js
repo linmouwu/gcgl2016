ad3146210a7c35db45445211c58be7d8e8311d2d
dingziran <dingziranrr@163.com>
2014-07-08 18:04:48 +0800

    prototype of new architecture

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,12 @@
 module.exports = function(grunt){
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-conventional-changelog');
     grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-html2js');
     grunt.registerTask('default', function(){
 		grunt.log.writeln('Hello from Grunt.');
 	});
@@ -10,12 +14,58 @@ module.exports = function(grunt){
     });
     var userConfig = require( './build.config.js' );
     var taskConfig = {
-        changelog: {
-            options: {
-                dest: 'CHANGELOG.md',
-                template: 'changelog.tpl'
+        /**
+         * We read in our `package.json` file so we can access the package name and
+         * version. It's already there, so we don't repeat ourselves here.
+         */
+        pkg: grunt.file.readJSON("package.json"),
+
+        /**
+         * The directories to delete when `grunt clean` is executed.
+         */
+        clean: [
+            '<%= build_dir %>',
+            '<%= compile_dir %>'
+        ],
+
+        /**
+         * HTML2JS is a Grunt plugin that takes all of your template files and
+         * places them into JavaScript files as strings that are added to
+         * AngularJS's template cache. This means that the templates too become
+         * part of the initial payload as one JavaScript file. Neat!
+         */
+        html2js: {
+            /**
+             * These are the templates from `src/app`.
+             */
+            app: {
+                options: {
+                    base: 'src/app'
+                },
+                src: [ '<%= app_files.atpl %>' ],
+                dest: '<%= build_dir %>/templates-app.js'
+            },
+
+            /**
+             * These are the templates from `src/common`.
+             */
+            common: {
+                options: {
+                    base: 'src/common'
+                },
+                src: [ '<%= app_files.ctpl %>' ],
+                dest: '<%= build_dir %>/templates-common.js'
             }
         },
+
+        /**
+         * `jshint` defines the rules of our linter as well as which files we
+         * should check. This file, all javascript sources, and all our unit tests
+         * are linted based on the policies listed in `options`. But we can also
+         * specify exclusionary patterns by prefixing them with an exclamation
+         * point (!); this is useful when code comes from a third party but is
+         * nonetheless inside `src/`.
+         */
         jshint: {
             src: [
                 '<%= app_files.js %>'
@@ -37,47 +87,195 @@ module.exports = function(grunt){
             },
             globals: {}
         },
+
         /**
-         * Increments the version number, etc.
+         * `grunt concat` concatenates multiple source files into a single file.
          */
-        bump: {
-            options: {
-                files: [
-                    "package.json",
-                    "bower.json"
+        concat: {
+            /**
+             * The `build_css` target concatenates compiled CSS and vendor CSS
+             * together.
+             */
+            build_css: {
+                src: [
+                    '<%= vendor_files.css %>',
+                    '<%= app_files.css %>'
                 ],
-                commit: false,
-                commitMessage: 'chore(release): v%VERSION%',
-                commitFiles: [
-                    "package.json",
-                    "client/bower.json"
+                dest: '<%= build_dir %>/assets/<%= pkg.name %>-<%= pkg.version %>.css'
+            },
+            /**
+             * The `compile_js` target is the concatenation of our application source
+             * code and all specified vendor source code into a single file.
+             */
+            compile_js: {
+                options: {
+                    banner: '<%= meta.banner %>'
+                },
+                src: [
+                    '<%= vendor_files.js %>',
+                    'module.prefix',
+                    '<%= build_dir %>/src/**/*.js',
+                    '<%= html2js.app.dest %>',
+                    '<%= html2js.common.dest %>',
+                    'module.suffix'
                 ],
-                createTag: false,
-                tagName: 'v%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: false,
-                pushTo: 'origin'
+                dest: '<%= compile_dir %>/assets/<%= pkg.name %>-<%= pkg.version %>.js'
             }
         },
+
         /**
-         * The Karma configurations.
+         * The `copy` task just copies files from A to B. We use it here to copy
+         * our project assets (images, fonts, etc.) and javascripts into
+         * `build_dir`, and then to copy the assets to `compile_dir`.
          */
-        karma: {
-            options: {
-                configFile: '<%= build_dir %>/karma-unit.js'
+        copy: {
+            build_app_assets: {
+                files: [
+                    {
+                        src: [ '**' ],
+                        dest: '<%= build_dir %>/assets/',
+                        cwd: 'src/assets',
+                        expand: true
+                    }
+                ]
             },
-            unit: {
-                port: 9019,
-                background: true
+            build_vendor_assets: {
+                files: [
+                    {
+                        src: [ '<%= vendor_files.assets %>' ],
+                        dest: '<%= build_dir %>/assets/',
+                        cwd: '.',
+                        expand: true,
+                        flatten: true
+                    }
+                ]
             },
-            continuous: {
-                singleRun: true
+            build_appjs: {
+                files: [
+                    {
+                        src: [ '<%= app_files.js %>' ],
+                        dest: '<%= build_dir %>/',
+                        cwd: '.',
+                        expand: true
+                    }
+                ]
+            },
+            build_vendorjs: {
+                files: [
+                    {
+                        src: [ '<%= vendor_files.js %>' ],
+                        dest: '<%= build_dir %>/',
+                        cwd: '.',
+                        expand: true
+                    }
+                ]
+            },
+            compile_assets: {
+                files: [
+                    {
+                        src: [ '**' ],
+                        dest: '<%= compile_dir %>/assets',
+                        cwd: '<%= build_dir %>/assets',
+                        expand: true
+                    }
+                ]
+            }
+        },
+
+        /**
+         * The `index` task compiles the `index.html` file as a Grunt template. CSS
+         * and JS files co-exist here but they get split apart later.
+         */
+        index: {
+
+            /**
+             * During development, we don't want to have wait for compilation,
+             * concatenation, minification, etc. So to avoid these steps, we simply
+             * add all script files directly to the `<head>` of `index.html`. The
+             * `src` property contains the list of included files.
+             */
+            build: {
+                dir: '<%= build_dir %>',
+                src: [
+                    '<%= vendor_files.js %>',
+                    '<%= build_dir %>/src/**/*.js',
+                    '<%= html2js.common.dest %>',
+                    '<%= html2js.app.dest %>',
+                    '<%= vendor_files.css %>',
+                    '<%= build_dir %>/assets/<%= pkg.name %>-<%= pkg.version %>.css'
+                ]
+            },
+
+            /**
+             * When it is time to have a completely compiled application, we can
+             * alter the above to include only a single JavaScript and a single CSS
+             * file. Now we're back!
+             */
+            compile: {
+                dir: '<%= compile_dir %>',
+                src: [
+                    '<%= concat.compile_js.dest %>',
+                    '<%= vendor_files.css %>',
+                    '<%= build_dir %>/assets/<%= pkg.name %>-<%= pkg.version %>.css'
+                ]
             }
         }
 
-
     };
 
+    grunt.registerTask( 'default', [ 'build' ] );
+
+    grunt.registerTask( 'build', [
+        'clean','html2js','concat:build_css','copy:build_app_assets','copy:build_vendor_assets',
+        'copy:build_appjs', 'copy:build_vendorjs', 'index:build'
+    ]);
+
+    /**
+     * The index.html template includes the stylesheet and javascript sources
+     * based on dynamic names calculated in this Gruntfile. This task assembles
+     * the list into variables for the template to use and then runs the
+     * compilation.
+     */
+    grunt.registerMultiTask( 'index', 'Process index.html template', function () {
+        var dirRE = new RegExp( '^('+grunt.config('build_dir')+'|'+grunt.config('compile_dir')+')\/', 'g' );
+        var jsFiles = filterForJS( this.filesSrc ).map( function ( file ) {
+            return file.replace( dirRE, '' );
+        });
+        var cssFiles = filterForCSS( this.filesSrc ).map( function ( file ) {
+            return file.replace( dirRE, '' );
+        });
+
+        grunt.file.copy('src/index.html', this.data.dir + '/index.html', {
+            process: function ( contents, path ) {
+                return grunt.template.process( contents, {
+                    data: {
+                        scripts: jsFiles,
+                        styles: cssFiles,
+                        version: grunt.config( 'pkg.version' )
+                    }
+                });
+            }
+        });
+    });
 
     grunt.initConfig( grunt.util._.extend( taskConfig, userConfig ) );
+
+    /**
+     * A utility function to get all app JavaScript sources.
+     */
+    function filterForJS ( files ) {
+        return files.filter( function ( file ) {
+            return file.match( /\.js$/ );
+        });
+    }
+
+    /**
+     * A utility function to get all app CSS sources.
+     */
+    function filterForCSS ( files ) {
+        return files.filter( function ( file ) {
+            return file.match( /\.css$/ );
+        });
+    }
+
 };
