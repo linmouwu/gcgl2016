aa73e15490429655d97699534f0dbfd98deef63e
dingziran <dingziranrr@163.com>
2014-07-10 10:45:47 +0800

    refactor(gcgl2016): make new whole grunt support

--- Gruntfile.js
+++ Gruntfile.js
@@ -414,7 +414,7 @@ module.exports = function(grunt){
     grunt.registerTask( 'build', [
         'clean','html2js','concat:build_css','copy:build_app_assets','copy:build_vendor_assets',
         'copy:build_appjs', 'copy:build_vendorjs',  'copy:build_vendorcss','copy:build_lib', 'index:build', 'karmaconfig',
-        'karma:continuous','bump-only'
+        'karma:continuous','bump-only:build','changelog'
     ]);
 
     /**
